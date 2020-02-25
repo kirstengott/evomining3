9efddd83b4a33b775069c88ab40cf2f413772956
Marc Chevrette <chevrm@gmail.com>
2020-02-25 11:16:47 -0600

    ani histogram plot edits

--- src/identify_ani_groups.py
+++ src/identify_ani_groups.py
@@ -45,16 +45,21 @@ for a in ani.ANI.tolist():
         ani_hist[onedec] = 1
 breaks = []
 i = min(ani_hist.keys()) - 0.1
-zero_flag = False
-first_zero = i
+zero_flag = False ## Are we in a sequence of zeroes?
+first_zero = i ## Where did we start? Initialized on i
+len_zero = 0 ## How many zeroes?
+min_zero = 3 ## Minimum allowed length of a sequence of zeroes to call a break
 while i <= 100:
     i = round(i,1)
     if i in ani_hist:
         if zero_flag:
-            last_zero = i - 0.1
-            breaks.append(round( (first_zero+last_zero) / 2, 2))
+            if len_zero >= min_zero:
+                last_zero = i - 0.1
+                breaks.append(round( (first_zero+last_zero) / 2, 2))
             zero_flag = False
+            len_zero = 0
     else:
+        len_zero += 1
         if not zero_flag:
             first_zero = i
             zero_flag = True
@@ -66,5 +71,8 @@ hist_plot = sb.distplot(ani['ANI'], bins=40)
 pdf = hist_plot.get_figure()
 for b in breaks:
     plt.axvline(x=b, linestyle='--', alpha=0.5, color='blue')
+    plt.text(b+0.1,0.1,str(b),rotation=90, size=8)
+plt.ylabel('Kernal density estimate')
+plt.xlabel('Average nucleotide identity')
 pdf.savefig("ani_histogram.pdf")
 plt.close()
